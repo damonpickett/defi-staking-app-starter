4a34af86b8a35609c2eee9558a57f16b561a6fca
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-02-25 10:28:52 -0800

    added token contracts and functionality

--- defi-staking-app-starter/src/contracts/RWD.sol
+++ defi-staking-app-starter/src/contracts/RWD.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
-contract Tether {
-    string public name = 'Mock Tether Token';
-    string public symbol = 'mUSDT';
+
+contract RWD {
+    string public name = 'Reward Token';
+    string public symbol = 'RWD';
     uint256 public totalSupply = 1000000000000000000000000; // ETH has 18 decimals
     uint8 public decimals = 18;
 
