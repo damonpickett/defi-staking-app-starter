c57a980fb07a922ece2d92ce40755d7aa20d9311
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-02 09:36:31 -0800

    we have begun to add functionality for staking of user deposits

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -8,8 +8,23 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public hisStaked;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+    }
+
+    // staking function
+    function depositTokens(uint _amount) public {
+        // Transfer Tether tokens to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        // Update Staking Balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount
     }
 }
