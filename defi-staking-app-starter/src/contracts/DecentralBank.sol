c447620e97c3382af59b3b3c42a760a0cd62e730
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-02 09:49:16 -0800

    added if statement to add user to hasStaked array if they have not staked

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -8,11 +8,13 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    // An array of people who have staked with this bank
     address[] public stakers;
 
+    // Key-value stores for the balance of a user's stake, and for keeping track of who has staked and is staked
     mapping(address => uint) public stakingBalance;
     mapping(address => bool) public hasStaked;
-    mapping(address => bool) public hisStaked;
+    mapping(address => bool) public isStaked;
 
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
@@ -25,6 +27,13 @@ contract DecentralBank {
         tether.transferFrom(msg.sender, address(this), _amount);
 
         // Update Staking Balance
-        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        if(!hasStaked) {
+            stakers.push[msg.sender];
+        }
+
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+
     }
 }
