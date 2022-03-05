47e33dbddfd1980783568dfb09e5cd7bf70cee56
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-04 21:00:07 -0800

    connected app to blockchain though not quite working properly

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
 import './App.css'
 import Navbar from './Navbar';
+import Web3 from 'web3';
 
 class App extends Component {
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockChainData()
+    }
+
+    // this connects our app to the blockchain
+    async loadWeb3() {
+        if(window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        } else if(window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+            } else {
+                window.alert('No ethereum browser detected. Check out MetaMask.')
+            }
+        }
+    
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const account = await web3.eth.getAccounts()
+        console.log(account)
+    }
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '0x0'
+        }
+    }
+
     render() {
         return (
             <div>
-                <Navbar />
+                <Navbar account={this.state.account}/>
             </div>
         )
     }
--- defi-staking-app-starter/src/components/Navbar.js
+++ defi-staking-app-starter/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import bank from '../bank.png'
+import bank from '../bank.png';
 
 
 class Navbar extends Component {
@@ -7,13 +7,13 @@ class Navbar extends Component {
         return (
             <nav className='navbar navbar-dark fixed-top shadow p-0' style ={{backgroundColor: 'black', height: '50px'}}>
                 <a className='navbar-brand col-sm-3 col-md-2 mr-0'
-                style={{color: 'white'}}><img src={bank} width='50' heigh='30' className='d-inline-block align-top' alt='bank image'/>
+                style={{color: 'white'}}><img src={bank} width='50' heigh='30' className='d-inline-block align-top' alt='bank'/>
                 &nbsp; DAPP Yield Staking (Decentralized Banking)
                 </a>    
                 <ul className='navbar-nav px-3'>
                     <li className='text-nowrap d-none nav-item d-sm-none d-sm-block'>
                         <small style={{color: 'white'}}>
-                            ACCOUNT NUMBER:
+                            ACCOUNT NUMBER: {this.props.account}
                         </small>
                     </li>
                 </ul>
