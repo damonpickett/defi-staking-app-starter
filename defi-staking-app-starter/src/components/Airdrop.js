6abcee17c2273c4146821058a16904d02c3db751
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-13 12:23:40 -0700

    created timer function in airdrop component

--- defi-staking-app-starter/src/components/Airdrop.js
+++ defi-staking-app-starter/src/components/Airdrop.js
@@ -14,6 +14,24 @@ class Airdrop extends Component {
         this.countDown = this.countDown.bind(this);
     }
 
+    secondsToTime(secs) {
+        let hours, minutes, seconds
+        hours = Math.floor(secs / (60 * 60))
+
+        let divisor_for_minutes = secs % (60 * 60)
+        minutes = Math.floor(divisor_for_minutes / 60)
+
+        let divisor_for_seconds = divisor_for_minutes % 60
+        seconds = Math.ceil(divisor_for_seconds)
+
+        let obj = {
+            'h': hours,
+            'm': minutes,
+            's': seconds
+        }
+        return obj
+    }
+
     render() {
         return (
             <div></div>
